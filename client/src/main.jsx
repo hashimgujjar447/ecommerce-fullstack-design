9a13a234253eebd4703555cac121632b918d1a19
Your Name <hashimgujjar4447@gmail.com>
2025-06-05 16:52:08 +0500

    Adding routing make pages and add tailwind libraries

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { Home, ProductList } from "./pages/index.js";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+      </Route>
+    </>,
+  ),
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
