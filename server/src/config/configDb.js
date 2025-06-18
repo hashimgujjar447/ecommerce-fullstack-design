cf51130c258e5760036b234c08470457ef20090c
Your Name <hashimgujjar4447@gmail.com>
2025-06-18 13:30:25 +0500

    Fix database config and update index.js

--- server/src/config/configDb.js
+++ server/src/config/configDb.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+console.log(process.env.MONGO_URI);
 async function connectToDatabase() {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ry7yplq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
-  );
+  await mongoose.connect(`${process.env.MONGO_URI}`);
 }
 
 export default connectToDatabase;
